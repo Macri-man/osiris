65c9f7daeac6701adffb0108b957f532d399bd99
Colin Davidson <colrdavidson@gmail.com>
2014-09-05 13:13:13 -0400

    Has sleep. MUCH ADVENTURE.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+#![feature(globs)]
+use std::io::timer;
+use std::time::*;
 fn main() {
-    println!("Hello, world!")
+    println!("This is a game. Prepare to have fun.");
+	timer::sleep(Duration::seconds(2));
+	println!("You're having fun now. Goodbye!");
 }
