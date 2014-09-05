d9361ddf1c484ad7c5135743f04fde740fd29547
Colin Davidson <colrdavidson@gmail.com>
2014-09-05 13:45:52 -0400

    UDP Thingy

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
-#![feature(globs)]
-use std::io::timer;
-use std::time::*;
+use std::io::net::udp::UdpSocket;
+use std::io::net::ip::{Ipv4Addr, SocketAddr};
+
 fn main() {
-    println!("This is a game. Prepare to have fun.");
-	timer::sleep(Duration::seconds(2));
-	println!("You're having fun now. Goodbye!");
+				let addr = SocketAddr {ip: Ipv4Addr(127, 0, 0, 1), port: 34000};
+				let mut socket = match UdpSocket::bind(addr) {
+								Ok(s) => s,
+								Err(e) => fail!("couldn't bind socket: {}", e),
+				};
+
+				let mut buf = [0, ..10];
+				match socket.recv_from(buf) {
+								Ok((amt, src)) => {
+												let buf = buf.mut_slice_to(amt);
+												buf.reverse();
+												socket.send_to(b"blargh", src);
+								}
+								Err(e) => println!("couldn't recieve a datagram: {}", e)
+				}
+				drop(socket);
 }
